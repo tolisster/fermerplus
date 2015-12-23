8765f16e763a44678fd8f8a6993943c8a2107d2c
aleksandr12345678 <rainbowclicks@mail.ru>
2015-12-23 14:46:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -38,147 +38,29 @@
        header("HTTP/1.1 404 Not Found");      
        exit(file_get_contents('./404.html'));	
 	}
+    
+    
+       $page = $GET['page'];
+
+
+  $fermer = array('main','about','services','contacts','seeds','all-seeds','seeds-disease','seeds-biology','fertilizers','all-fertilizers','fertilizers-products','peat','news','pellicle','articles','contacts','search');
         
-
-         
-   $page = $GET['page'];
+           
+  if(!in_array($page,$fermer)) { include './modules/main/router.php'; break;}
         
-
-    switch($GET['page'])  {
-   
-        case 'main':
-		include './modules/main_controller.php';
-		include './modules/engine.php';
-		include './modules/main/router.php';
-        break;
-		
-		
-		case 'about':
-		include './modules/about_controller.php';
-		include './modules/engine.php';
-		include './modules/about/router.php';
-        break;
-		
-		case 'services':
-		include './modules/services_controller.php';
-		include './modules/engine.php';
-		include './modules/services/router.php';
-        break;
-		
-		case 'contacts':
-		include './modules/contacts_controller.php';
-		include './modules/engine.php';
-		include './modules/contacts/router.php';
-        break;
-		
-		case 'seeds':
-		 include './modules/seeds_controller.php';
-		
-			  include './modules/engine.php';
-			 
-			  include './modules/seeds/router.php';  
-        break;
-     
-	 	case 'all-seeds':
-		include './modules/all-seeds_controller.php';
-			  include './modules/engine.php';
-			  
-			  include './modules/all-seeds/router.php';  
-        break;
-		
-		case 'seeds-disease':
-		include './modules/seeds-disease_controller.php';
-		include './modules/engine.php';
-		include './modules/seeds-disease/router.php';  
-        break;
-		
-		case 'seeds-biology':
-		 include './modules/seeds-biology_controller.php';
-	     include './modules/engine.php';
-		 include './modules/seeds-biology/router.php';  
-        break;
-		
-		case 'fertilizers':
-		      include './modules/fertilizers_controller.php';
-			  include './modules/engine.php';
-			  include './modules/fertilizers/router.php';  
-        break;
-		
-		case 'all-fertilizers':
-			  include './modules/all-fertilizers_controller.php';
-			  include './modules/engine.php';
-
-			  include './modules/all-fertilizers/router.php';  
-        break;
-		
-		case 'fertilizers-products':
-		include './modules/fertilizers-products_controller.php';
-			  include './modules/engine.php';
-			  
-			  include './modules/fertilizers-products/router.php';  
-        break;
-		
-		case 'peat':
-		 include './modules/peat_controller.php';
-			  include './modules/engine.php';
-			 
-			  include './modules/peat/router.php';  
-        break;
-		
-		
-		
-		case 'news':
-			 
-			  include './modules/news_controller.php';
-			   include './modules/engine.php';
-			  include './modules/news/router.php';  
-        break;
-		
-		
-		
-		
-		case 'pellicle':
-		include './modules/pellicle_controller.php';
-			  include './modules/engine.php';
-			  
-			  include './modules/pellicle/router.php';  
-        break;
-		
-		case 'articles':
-		 include './modules/articles_controller.php';
-			  include './modules/engine.php';
-			 
-			  include './modules/articles/router.php';  
-        break;
-		
-		
-		case 'contacts':
-		include './modules/contacts_controller.php';
-			  include './modules/engine.php';
-			  
-			  include './modules/contacts/router.php';  
-              
-        break;
-		
-		
-		case 'search':
-		      include './modules/search_controller.php';
-			  include './modules/engine.php';
-              include './modules/search/router.php';  
-        break;
-		
-
-
-		
-
-
-
-     default: 
-           include './modules/main/router.php';  
-        break;     
-    } 
-
-    $content = ob_get_contents();  
+    foreach($fermer as $ferm)  {  
+           
+  if($page == $ferm){
+   		 
+             include './modules/'.$ferm.'_controller.php';
+             include './modules/engine.php';
+             include './modules/'.$ferm.'/router.php';  
+             break;
+    
+ }
+}
+ 
+  $content = ob_get_contents();  
   
     ob_end_clean();
 
